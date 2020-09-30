b9a29290a82de19ff6dd33461909d3676d2dac7d
markulie <66760217+markulie@users.noreply.github.com>
2020-09-30 18:54:20 +0400

    Initial

---
+++ ShutDownLater/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace ShutDownLater
+{
+    public partial class FormMain : Form
+    {
+        DateTime mShutdownTime;
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonRun_Click(object sender, EventArgs e)
+        {
+            mShutdownTime = DateTime.Now.AddHours(1);
+            labelInfo.Visible = true;
+            labelInfo.Text = "";
+            timer.Start();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (DateTime.Now < mShutdownTime)
+            {
+                TimeSpan ts = mShutdownTime - DateTime.Now;
+                labelInfo.Text = "" + ts.Minutes + " minutes " + ts.Seconds + " seconds ";
+            }
+
+            else
+            {
+                Close();
+                Process.Start("shutdown.exe", "-h");
+            }
+        }
+
+        private void labelInfo_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
